431508dc44226c6ec29ed363c1b293465c59feda
gcrick <geli.crick@osoco.es>
2016-05-24 12:18:58 +0200

    Hello world using Kotatsu, Webpack, Babel, and React

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # coupons-poc-reactjs
 React.js front end for coupon app proof of concept
+
+## Development
+
+Start a development server with:
+
+    npm start
+
+Build the JavaScript for production with:
+
+    npm run-script build
+
+## Configuration
+
+* **package.json**: npm project configuration file. Defines scripts for running and building the project using Kotatsu. 
+
+* **webpack.config.js**: Webpack configuration file, passed to Kotatsu.
+
+* **.babelrc**: Babel configuration file used by Kotatsu and Webpack for compiling JavaScript.
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const CleanPlugin = require('clean-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+  style: path.join(__dirname, 'app/main.css')
+};
+
+const common = {
+  entry: [path.join(PATHS.app, 'main.jsx'), PATHS.style],
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }
+    ]
+  }
+};
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {
+    output: {
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[chunkhash].js'
+    },
+    plugins: [
+      new CleanPlugin([PATHS.build]),
+      // Setting DefinePlugin affects React library size!
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"production"'
+      })
+    ]
+  });
+}
+else {
+  module.exports = common;
+}
